481187cc3fb04067643a9ad55d7f58cdad147e6e
Hugaso161 <99998251+Hugaso161@users.noreply.github.com>
2024-05-29 19:00:06 -0400

    Update nombre_3_vocales.py

--- nombre_3_vocales.py
+++ nombre_3_vocales.py
@@ -1,1 +1,13 @@
+vocales = ["a","e","i","o","u"]
+nombre = input("Escribe el nombre: \n")
+numero_vocales = 0
+
+for item in list(nombre):
+  if item in vocales:
+    numero_vocales += 1
+
+if numero_vocales >= 3:
+  print("\n"+nombre+" tiene "+ str(numero_vocales)+" vocales")
+else:
+  print("No tiene 3 vocales")
 
